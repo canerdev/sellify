b38db86a2eb379ba57f18bbe2e18e49a24a3b887
yahyayesilyurt <yesilyurty21@itu.edu.tr>
2024-11-03 10:34:31 +0300

    schema sql file refactor

--- backend/app/db/schema.sql
+++ backend/app/db/schema.sql
@@ -14,24 +14,59 @@
 --    FOREIGN KEY (user_id) REFERENCES users(id)
 --);
 
+-- categories
 CREATE TABLE IF NOT EXISTS categories (
-    categoryID INT PRIMARY KEY AUTOINCREMENT,
+    categoryID INT PRIMARY KEY AUTO_INCREMENT,
     categoryName VARCHAR(50) NOT NULL,
     subCategory VARCHAR(50) NOT NULL,
     categoryDescription TEXT,
     activeStatus BOOLEAN NOT NULL DEFAULT TRUE
 );
 
+-- products
+CREATE TABLE IF NOT EXISTS products (
+    productID VARCHAR(15) NOT NULL PRIMARY KEY,
+    categoryID INT NOT NULL,
+    productName VARCHAR(100) NOT NULL,
+    price DECIMAL(19, 4) NOT NULL,
+    stockCount INT NOT NULL,
+    lastSold DATE NOT NULL,
+    FOREIGN KEY (categoryID) REFERENCES categories(categoryID)
+);
+
+-- departments
+CREATE TABLE IF NOT EXISTS departments (
+    departmentID INT PRIMARY KEY AUTO_INCREMENT,
+    departmentName VARCHAR(50) NOT NULL UNIQUE,
+    employeeCount SMALLINT NOT NULL DEFAULT 0
+);
+
+-- employees
 CREATE TABLE IF NOT EXISTS employees (
-    employeeID INT PRIMARY KEY AUTOINCREMENT,
+    employeeID INT PRIMARY KEY AUTO_INCREMENT,
     employeeName VARCHAR(50) NOT NULL,
     gender VARCHAR(10) NOT NULL,
     startDate DATETIME NOT NULL,
     country VARCHAR(50) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     age INT NOT NULL,
-    departmentID SMALLINT NOT NULL,
+    departmentID INT NOT NULL,
     FOREIGN KEY (departmentID) REFERENCES departments(departmentID)
+);
+
+-- customers
+CREATE TABLE IF NOT EXISTS customers (
+    customerID VARCHAR(8) NOT NULL PRIMARY KEY,
+    lastOrderID VARCHAR(14),
+    customerName VARCHAR(50) NOT NULL,
+    segment VARCHAR(20) NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state VARCHAR(50) NOT NULL,
+    postalCode INT NOT NULL,
+    region VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
 -- orders
@@ -52,36 +87,9 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (productID) REFERENCES products(productID)
 );
 
--- departments
-CREATE TABLE IF NOT EXISTS departments (
-    departmentID INT PRIMARY KEY AUTOINCREMENT,
-    departmentName VARCHAR(50) NOT NULL UNIQUE,
-    employeeCount SMALLINT NOT NULL DEFAULT 0
-);
+ALTER TABLE customers
+ADD CONSTRAINT fk_lastOrderID FOREIGN KEY (lastOrderID) REFERENCES orders(orderID);
 
--- customers
-CREATE TABLE IF NOT EXISTS customers (
-    customerID VARCHAR(8) NOT NULL PRIMARY KEY,
-    lastOrderID VARCHAR(14),
-    customerName VARCHAR(50) NOT NULL,
-    segment VARCHAR(20) NOT NULL,
-    country VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(50) NOT NULL,
-    postalCode INT NOT NULL,
-    region VARCHAR(50) NOT NULL,
-    phone VARCHAR(20) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    FOREIGN KEY (lastOrderID) REFERENCES orders(orderID),
-);
 
--- products
-CREATE TABLE IF NOT EXISTS products (
-    productID VARCHAR(15) NOT NULL PRIMARY KEY,
-    categoryID INT NOT NULL,
-    productName VARCHAR(100) NOT NULL,
-    price DECIMAL(19, 4) NOT NULL,
-    stockCount INT NOT NULL,
-    lastSold DATE NOT NULL,
-    FOREIGN KEY (categoryID) REFERENCES categories(categoryID)
-);
+
+
