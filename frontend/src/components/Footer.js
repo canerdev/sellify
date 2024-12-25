f7899805f56cb5caae8381503b1b63f2cceedae1
Caner Aslan <canerdevx@gmail.com>
2024-12-25 18:56:35 +0300

    remove logo in the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,15 +8,6 @@ export default function Footer() {
     <footer className="bg-[#13243e] p-4 text-center text-white">
       <div className="flex flex-col items-center space-y-2">
         <div className="flex items-center space-x-2">
-          <Link href="#">
-            <Image
-              className="w-auto h-12 rounded-md"
-              width={150}
-              height={30}
-              src="/sellify-logo.png"
-              alt="Sellify Logo"
-            />
-          </Link>
           <p className="text-gray-400 text-sm">
             © {year} Sellify. All rights reserved.
           </p>
